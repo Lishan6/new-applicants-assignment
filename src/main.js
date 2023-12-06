e291488fbb3535725f3b84b653509aa66586985c
Lishan6 <lishangebremariam16@gmail.com>
2023-12-06 14:59:50 +0100

    add javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-// Add your JavaScript here
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.querySelector('.display');
+    const buttons = document.querySelectorAll('.buttons button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const buttonText = button.textContent;
+
+            switch (buttonText) {
+                case 'AC':
+                    display.value = '';
+                    break;
+                case 'DEL':
+                    display.value = display.value.slice(0, -1);
+                    break;
+                case '=':
+                    try {
+                        display.value = eval(display.value);
+                    } catch (error) {
+                        display.value = 'Error';
+                    }
+                    break;
+                default:
+                    display.value += buttonText;
+                    break;
+            }
+        });
+    });
+});
